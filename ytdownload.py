23a50b4ad6b524413e13312dd14121a08aadb11e
Kelvin Maina <99181743+KMaina-N@users.noreply.github.com>
2022-05-29 21:46:17 +0200

    Add files via upload

---
+++ ytdownload.py
@@ -0,0 +1,25 @@
+
+from pytube import YouTube
+import os
+
+# url input from user
+yt = YouTube(
+	str(input("Enter the URL of the video you want to download: \n>> ")))
+
+# extract only audio
+video = yt.streams.get_highest_resolution()
+
+# check for destination to save file
+print("Enter the destination (leave blank for current directory)")
+destination = str(input(">> ")) or '.'
+
+# download the file
+out_file = video.download(output_path=destination)
+
+# save the file
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp4'
+os.rename(out_file, new_file)
+
+# result of success
+print(yt.title + " has been successfully downloaded.")
